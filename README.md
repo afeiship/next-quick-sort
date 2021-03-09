16c1c0037fc170cb2f5e639dd83e07333ade7165
afeiship <1290657123@qq.com>
2021-03-09 09:53:16 +0800

    docs: fix docs

--- README.md
+++ README.md
@@ -15,7 +15,9 @@ npm install -S @jswork/next-quick-sort
 ```js
 import '@jswork/next-quick-sort';
 
-//DOCS here!
+const array = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
+nx.quickSort(array);
+// [2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
 ```
 
 ## license
---
+++ dist/index.js
@@ -0,0 +1,43 @@
+/*!
+ * name: @jswork/next-quick-sort
+ * description: Quick sort for next.
+ * homepage: https://github.com/afeiship/next-quick-sort
+ * version: 1.0.0
+ * date: 2021-03-09 09:51:34
+ * license: MIT
+ */
+
+(function () {
+  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
+  var nx = global.nx || require('@jswork/next');
+
+  nx.quickSort = function (inArray) {
+    var length = inArray.length;
+    if (length <= 1) return inArray;
+
+    var pivotIdx = Math.floor(length / 2);
+    var pivot = inArray[pivotIdx];
+    var left = [];
+    var right = [];
+
+    for (var i = 0; i < length; i++) {
+      var value = inArray[i];
+      if (value < pivot) {
+        left.push(value);
+      }
+
+      if (value > pivot) {
+        right.push(value);
+      }
+    }
+    var start = nx.quickSort(left);
+    var middle = [pivot];
+    var end = nx.quickSort(right);
+
+    return [].concat(start, middle, end);
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.quickSort;
+  }
+})();
